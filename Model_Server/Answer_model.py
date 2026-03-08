4a7c800d7e46393b505d3b461f4f1b79be74ece7
Szymon Drywa <szmekdrywa@wp.pl>
2026-03-08 13:47:29 +0100

    final repository

--- Model_Server/Answer_model.py
+++ Model_Server/Answer_model.py
@@ -28,7 +28,7 @@ def rag_query(prompt,query):
     
     prompt = f"{prompt}\n{context}\n{query}\nPodsumowanie:"
     resp = llm(prompt,
-               max_tokens=500,
+               max_tokens=1000,
                temperature=0.7,
                stop=None,
                echo=False)
